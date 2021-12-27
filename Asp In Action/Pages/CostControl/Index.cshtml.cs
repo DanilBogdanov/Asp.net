a7a6c0d4ca443585d77b4598f40d11174721fbe0
Danil HP <bogdanovdanila1404@gmail.com>
2021-12-27 20:24:44 +0500

    add page My

---
+++ Asp In Action/Pages/CostControl/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Asp_In_Action.Pages.CostControl
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
