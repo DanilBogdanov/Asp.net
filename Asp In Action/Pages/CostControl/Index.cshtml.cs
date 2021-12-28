5481f18518747cb59e2db317cfec98831030f6e6
Danil HP <bogdanovdanila1404@gmail.com>
2021-12-28 21:38:33 +0500

    try get post

--- Asp In Action/Pages/CostControl/Index.cshtml.cs
+++ Asp In Action/Pages/CostControl/Index.cshtml.cs
@@ -1,12 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
 
 namespace Asp_In_Action.Pages.CostControl
 {
     public class IndexModel : PageModel
     {
+        public string Message { get; set; }
+        public string Type { get; set; }
         public void OnGet()
         {
+            Message = "OnGet";
         }
+
+        public void OnPost()
+        {
+            Message = "OnPost";
+            Debug.WriteLine(Message);
+        }
+
+        //public void OnPostAddExpense()
+        //{
+        //    Message = "OnPostAddExpense";
+        //} 
     }
 }
