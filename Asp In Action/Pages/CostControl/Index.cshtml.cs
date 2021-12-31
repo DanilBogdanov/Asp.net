78b8a97c14944d52b9b3c81ccc05fc495844fc39
Danil HP <bogdanovdanila1404@gmail.com>
2021-12-31 19:36:54 +0500

    post self

--- Asp In Action/Pages/CostControl/Index.cshtml.cs
+++ Asp In Action/Pages/CostControl/Index.cshtml.cs
@@ -6,17 +6,21 @@ namespace Asp_In_Action.Pages.CostControl
 {
     public class IndexModel : PageModel
     {
+        [BindProperty(SupportsGet = true)]
         public string Message { get; set; }
         public string Type { get; set; }
+        public string HttpMethod { get; set; }
+        [BindProperty(SupportsGet =true)]
+        public string Text { get; set; }
         public void OnGet()
         {
-            Message = "OnGet";
+            HttpMethod = "OnGet";
         }
 
         public void OnPost()
         {
-            Message = "OnPost";
-            Debug.WriteLine(Message);
+            HttpMethod = "OnPost";
+            Debug.WriteLine(HttpMethod);
         }
 
         //public void OnPostAddExpense()
