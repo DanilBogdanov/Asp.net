63abfe42517e68716a0ea19864226685be9d2677
Danil Work Win7 <bogdanovdanila1404@gmail.com>
2022-01-12 15:56:41 +0500

    with hiden

---
+++ ForTestConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ForTestConsole
+{
+    class Program
+    {
+        const int a = 1;
+#nullable enable
+        static void Main(string[] args)
+        {
+            Guid guid = Guid.NewGuid();
+            Guid guid1 = Guid.NewGuid();
+            Console.WriteLine(guid);
+            Console.WriteLine(guid1);
+            string a = "a";
+            string b = a;
+            Console.WriteLine(a == b);
+            b = b + "1";
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
