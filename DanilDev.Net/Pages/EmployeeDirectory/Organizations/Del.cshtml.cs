f1c03795e7c8d207d86bf4bf5297efc49281a747
Danil HP <bogdanovdanila1404@gmail.com>
2022-02-18 11:22:22 +0500

    add pages add and del to organizations

---
+++ DanilDev.Net/Pages/EmployeeDirectory/Organizations/Del.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DanilDev.Pages.EmployeeDirectory.Organizations
+{
+    public class DelModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
